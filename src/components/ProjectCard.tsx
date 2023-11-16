a95fc541e58074eab17f5a5b029a3f21af7c14f6
Kino <andresprza@gmail.com>
2023-11-16 17:16:50 -0600

    Refactor ProjectCard component layout + Day 1 + Day 2

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,18 +11,18 @@ interface Project {
 export default function ProjectCard({day, title, image, href} : Project): JSX.Element {
 
   return <Atropos rotateTouch={false}>
-  <a className={`w-full h-full grid place-items-center overflow-hidden
-  bg-[radial-gradient(_circle,_rgba(0,_0,_0,_0.6783088235294117)_2%,_rgba(0,_0,_0,_0.01724439775910369)_100%)]
-  hover:bg-[linear-gradient(_0deg,_rgba(0,_0,_0,_0.6334908963585435)_0%,_rgba(0,_0,_0,_0)_44%,_rgba(0,_0,_0,_0)_100%)]
-  `}
-  href={href}>
-    <img className='w-full h-full object' src={image} alt={title}/>
-    <div className='absolute cover grid place-items-center overflow-hidden h-full w-full'>
-      <div className='relative w-full h-full grid place-items-center group'>
-        <h1 className='text-3xl absolute transition-all group-hover:opacity-0 group-hover:-translate-y-12'>Day {day}</h1>
-        <h1 className='text-lg md:text-2xl lg:text-3xl absolute bottom-4 transition-all translate-y-32 group-hover:translate-y-0'>{title}</h1>
+    <a className='w-full h-full grid place-items-center overflow-hidden'
+    href={href}>
+      <img className='w-full h-full object' src={image} alt={title}/>
+      <div className={`absolute cover grid place-items-center overflow-hidden h-full w-full
+      bg-[radial-gradient(_circle,_rgba(0,_0,_0,_0.6783088235294117)_2%,_rgba(0,_0,_0,_0.01724439775910369)_100%)]
+      hover:bg-[linear-gradient(_0deg,_rgba(0,_0,_0,_0.6334908963585435)_0%,_rgba(0,_0,_0,_0)_44%,_rgba(0,_0,_0,_0)_100%)]
+      `}>
+        <div className='relative w-full h-full grid place-items-center group'>
+          <h1 className='text-3xl absolute transition-all group-hover:opacity-0 group-hover:-translate-y-12'>Day {day}</h1>
+          <h1 className='text-lg md:text-2xl lg:text-3xl absolute bottom-4 transition-all translate-y-32 group-hover:translate-y-0'>{title}</h1>
+        </div>
       </div>
-    </div>
-  </a>
-</Atropos>
+    </a>
+  </Atropos>
 }
---
+++ src/components/day/2/Progress.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react'
+import { TbPlayerTrackNextFilled } from 'react-icons/tb'
+import { HiFire, HiEye, HiMusicNote, HiPuzzle, HiStar, HiHeart, HiCube } from 'react-icons/hi'
+const steps = [
+  {
+    icon: <HiFire />
+  },
+  {
+    icon: <HiEye />
+  },
+  {
+    icon: <HiMusicNote />
+  },
+  {
+    icon: <HiPuzzle />
+  },
+  {
+    icon: <HiStar />
+  },
+  {
+    icon: <HiHeart />
+  },
+  {
+    icon: <HiCube />
+  }
+]
+
+const ProgressBar = ({
+  className,
+  percentage
+}: {
+  className: string
+  percentage: number | null
+}): JSX.Element => (
+  <svg
+    fill='none'
+    className={`aspect-square transition-all ${className}`}
+    viewBox='-10 -10 130 130'
+    style={{
+      strokeDasharray:
+        percentage != null
+          ? `calc(${percentage} * 251.327412 / 100) 9999`
+          : '0 20.9415 0 41.883 0 41.883 0 41.883 0 41.883 0 41.883 0 41.883'
+    }}>
+    <path
+      d='m 25.3892626 95.4508497 C 25.3892626 95.4508497 5 82.6142375 5 55 C 5 27.3857625 27.3857625 5 55 5 C 82.6142375 5 105 27.3857625 105 55 C 105 82.6142375 84.3892626 95.4508497 84.3892626 95.4508497'
+      strokeWidth={percentage != null ? '6' : '3'}
+      strokeLinecap='round'></path>
+  </svg>
+)
+
+export default function Progress(): JSX.Element {
+  const n = steps.length
+  const [progress, setProgress] = useState(0)
+  const percentage = (progress / (n - 1)) * 100
+  const circumference = 360 * 0.8
+
+  const addOne = (): void => {
+    setProgress(() => (progress + 1) % n)
+  }
+
+  const onStepPress = (index: number) => () => {
+    setProgress(index)
+  }
+  return (
+    <div className='h-screen grid place-items-center relative'>
+      <ProgressBar className='absolute stroke-white/5 -z-10 h-[80vmin]' percentage={null} />
+      <ProgressBar className={`absolute stroke-zinc-700 h-[80vmin]`} percentage={percentage} />
+
+      {steps.map((step, index) => {
+        const angle = circumference / 2 - ((n - 1 - index) * circumference) / (n - 1)
+        const done = index <= progress
+        return (
+          <button
+            onClick={onStepPress(index)}
+            onDragEnter={e => {
+              e.preventDefault()
+              e.stopPropagation()
+              onStepPress(index)()
+            }}
+            onDragStart={e => {
+              e.preventDefault()
+              e.stopPropagation()
+              onStepPress(index)()
+            }}
+            key={`step-${index}`}
+            id={`step-${index}`}
+            className='absolute step aspect-square h-[25vmin] rounded-full grid place-items-center transition-all'
+            style={{
+              transform: `rotate(${angle}deg) translateY(-30.7692vmin) rotate(calc(0deg - ${angle}deg))`
+            }}>
+            <span
+              className={`text-lg grid place-items-center transition-all p-2 border-zinc-700 rounded-full aspect-square h-14 w-14
+              ${done ? 'bg-zinc-700 opacity-100 border-8 md:scale-150' : 'opacity-40 border-0'}
+            `}
+              style={
+                !done
+                  ? {}
+                  : {
+                      boxShadow: 'inset -0.2rem 0.2rem 5px #00000022, inset 0.2rem -0.2rem 5px #ffffff22'
+                    }
+              }>
+              {step.icon}
+            </span>
+          </button>
+        )
+      })}
+
+      <button
+        onClick={addOne}
+        className='z-10 aspect-square h-20 rounded-full flex items-center justify-center text-black/30 text-4xl'
+        style={{
+          boxShadow:
+            'inset -0.2rem 0.2rem 5px #00000022, inset 0.2rem -0.2rem 5px #ffffff12, -0.2rem 0.2rem 15px #00000062, 0.2rem -0.2rem 15px #ffffff12'
+        }}>
+        <TbPlayerTrackNextFilled />
+      </button>
+    </div>
+  )
+}
