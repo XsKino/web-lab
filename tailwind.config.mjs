c5492946e5ddabbffc0b320c912e50f49d4e7599
Kino <andresprza@gmail.com>
2023-11-16 21:43:50 -0600

    rotating navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,28 +2,25 @@ import { nextui } from '@nextui-org/react'
 import { darkThemeColors, lightThemeColors } from './src/styles/colorThemes'
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
+  content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
-	theme: {
-		extend: {},
-	},
-	darkMode: 'class',
-  plugins: [nextui(
-		{
-			addCommonColors: false,
-			themes:
-				{
-					dark: {
-						colors:
-							darkThemeColors
-					},
-					light: {
-						colors:
-							lightThemeColors
-					}
-				}
-		}
-	)]
+  theme: {
+    extend: {}
+  },
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      addCommonColors: true,
+      themes: {
+        dark: {
+          colors: darkThemeColors
+        },
+        light: {
+          colors: lightThemeColors
+        }
+      }
+    })
+  ]
 }
