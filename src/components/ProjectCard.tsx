903919b35467688b864685ff37ba160454628093
Kino <andresprza@gmail.com>
2023-11-21 23:38:28 -0600

    Day 7

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,10 +11,14 @@ interface Project {
 export default function ProjectCard({ test, title, image, href }: Project): JSX.Element {
   return (
     <Atropos rotateTouch={false}>
-      <a className='w-full h-full grid place-items-center overflow-hidden' href={href}>
-        <img className='w-full h-full object' src={image} alt={title} />
+      <a className='group w-full h-full grid place-items-center overflow-hidden ' href={href}>
+        <img
+          className='scale-110 group-hover:scale-100 transition-all w-full h-full object'
+          src={image}
+          alt={title}
+        />
         <div
-          className={`absolute cover grid place-items-center overflow-hidden h-full w-full
+          className={`absolute backdrop-blur-[2px] backdrop-grayscale hover:backdrop-filter-none transition-all cover grid place-items-center overflow-hidden h-full w-full
       bg-[radial-gradient(_circle,_rgba(0,_0,_0,_0.6783088235294117)_2%,_rgba(0,_0,_0,_0.01724439775910369)_100%)]
       hover:bg-[linear-gradient(_0deg,_rgba(0,_0,_0,_0.6334908963585435)_0%,_rgba(0,_0,_0,_0)_44%,_rgba(0,_0,_0,_0)_100%)]
       `}>
@@ -22,7 +26,7 @@ export default function ProjectCard({ test, title, image, href }: Project): JSX.
             <h1 className='text-3xl absolute transition-all group-hover:opacity-0 group-hover:-translate-y-12'>
               test {test}
             </h1>
-            <h1 className='text-lg md:text-2xl lg:text-3xl absolute bottom-4 transition-all translate-y-32 group-hover:translate-y-0'>
+            <h1 className='font-["Indie_Flower"] text-lg md:text-2xl lg:text-3xl absolute bottom-4 transition-all translate-y-32 group-hover:translate-y-0'>
               {title}
             </h1>
           </div>
