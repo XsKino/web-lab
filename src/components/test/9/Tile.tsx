f38ef7aa185a770fdf7b9021b091cc5d1a37b826
Kino <andresprza@gmail.com>
2023-12-02 15:53:16 -0600

    Day 9

---
+++ src/components/test/9/Tile.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Button } from '@nextui-org/button'
+
+export default function Tile({
+  children,
+  sound,
+  volume = 0.2
+}: {
+  children: React.ReactNode
+  sound: string
+  volume?: number
+}): JSX.Element {
+  try {
+    const audio = new Audio(sound)
+    audio.volume = volume
+
+    const handleClick = (): void => {
+      audio.currentTime = 0
+      audio
+        .play()
+        .then(() => {
+          console.log('played', sound)
+          audio.currentTime = 0
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    return (
+      <Button
+        color='primary'
+        variant='ghost'
+        onClick={handleClick}
+        isIconOnly
+        className='w-full h-full text-[6vmin]'>
+        {children}
+      </Button>
+    )
+  } catch (err) {
+    console.log(err)
+
+    return (
+      <div className='border border-red-500 rounded-md text-red-500 text-[7vmin] grid place-items-center rotate-90'>
+        <div className='border border-red-500 border-b-transparent rounded-full p-[3vmin] animate-spinner-ease-spin' />
+      </div>
+    )
+  }
+}
