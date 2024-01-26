053d10596146e7b9c8be41831dfe9a43bafdbd7e
Kino <andresprza@gmail.com>
2024-01-26 14:22:38 -0600

    Add Vue and Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
+import vue from '@astrojs/vue'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()]
-});
+  integrations: [react(), vue(), svelte(), tailwind()]
+})
